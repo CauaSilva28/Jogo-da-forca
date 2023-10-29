3871fcb223174f0629050663e4293dc58a26cace
CauaSilva28 <caua.santos56@etec.sp.gov.br>
2023-10-29 13:26:01 -0300

    Criando jogo da forca

--- js/Boneco.js
+++ js/Boneco.js
@@ -4,44 +4,69 @@ class Boneco{
     }
     
     cabeca(){
-        this.ctx.lineWidth = 10;
-        this.ctx.beginPath();
-        this.ctx.arc(250,300,50,0,Math.PI*2);
-        this.ctx.stroke();
+        var img = new Image();
+        img.src = "img/cabeca.png";
+        img.onload = function(){
+            ctx.drawImage(img, 140, 230, 150, 150);
+        }
     }
     
     tronco(){
+        this.ctx.lineWidth = 10;
         this.ctx.beginPath();
-        this.ctx.moveTo(250,350);
-        this.ctx.lineTo(250,500);
+        this.ctx.moveTo(220,375);
+        this.ctx.lineTo(220,500);
         this.ctx.stroke();
     }
 
     bracoDireito(){
         this.ctx.beginPath();
-        this.ctx.moveTo(250,380);
-        this.ctx.lineTo(320,430);
+        this.ctx.moveTo(220,380);
+        this.ctx.lineTo(290,430);
         this.ctx.stroke();
     }
 
     bracoEsquerdo(){
         this.ctx.beginPath();
-        this.ctx.moveTo(250,380);
-        this.ctx.lineTo(180,430);
+        this.ctx.moveTo(220,380);
+        this.ctx.lineTo(150,430);
         this.ctx.stroke();
     }
 
     pernaDireita(){
         this.ctx.beginPath();
-        this.ctx.moveTo(250,500);
-        this.ctx.lineTo(290,580);
+        this.ctx.moveTo(220,500);
+        this.ctx.lineTo(260,580);
         this.ctx.stroke();
     }
 
     pernaEsquerda(){
         this.ctx.beginPath();
-        this.ctx.moveTo(250,500);
-        this.ctx.lineTo(210,580);
+        this.ctx.moveTo(220,500);
+        this.ctx.lineTo(180,580);
         this.ctx.stroke();
+    }
+
+    desenhaBoneco(erros){
+        switch(erros){
+            case 1:
+                this.cabeca();
+                break;
+            case 2:
+                this.tronco();
+                break;
+            case 3:
+                this.bracoDireito();
+                break;
+            case 4:
+                this.bracoEsquerdo();
+                break;
+             case 5:
+                this.pernaDireita();
+                break;
+            case 6:
+                this.pernaEsquerda();
+                break;
+        }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,27 +1,72 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-const palavras = ['CASA', 'JOGOS', 'PROFESSORA', 'ABACAXI', 'ADVOGADO'];
-var palavraSecreta = palavras[Math.floor(Math.random() * 6)];
-var tentativas = '';
+const palavras = ['MUSSARELO', 'NETFLIXO', 'NIVIO', 'SAMSUNGO', 'CARAPICUIBO', 'CAPIVARO', 'BORBOLETO', 'LUDMILO', 'CARAGUATATUBO', 'IPHONO'];
+var erros = 0;
+var acertos = 0;
+var tentativas = "";
+const palavraEscolhida = palavras[Math.floor(Math.random() * 10)];
+var numLetras = palavraEscolhida.length;
 
 const boneco = new Boneco(ctx);
-
-boneco.cabeca();
-boneco.tronco();
-boneco.bracoDireito();
-boneco.bracoEsquerdo();
-boneco.pernaDireita();
-boneco.pernaEsquerda();
 
 function cabo(){
     ctx.lineWidth = 10;
     ctx.beginPath();
     ctx.moveTo(100,600);
     ctx.lineTo(100,200);
-    ctx.lineTo(250,200);
-    ctx.lineTo(250,250);
+    ctx.lineTo(220,200);
+    ctx.lineTo(220,250);
     ctx.stroke();
 }
 
+function linhasLetra(){
+    for(var i = 0; i < numLetras; i++){
+        ctx.beginPath();
+        ctx.moveTo((i+4)*81, 600);
+        ctx.lineTo((i+5)*80, 600);
+        ctx.stroke();
+    }
+}
+
+function letraEscolhida(){
+    if(tentativas.includes(event.key)){
+        alert("Essa letra ja foi escolhida!");
+    }
+
+    if(!tentativas.includes(event.key) && palavraEscolhida.includes((event.key).toUpperCase())){
+        letrasRepetidas();
+        for(var i = 0; i < numLetras; i++){
+            if(palavraEscolhida[i] == (event.key).toUpperCase()){
+                ctx.fillStyle = "#fff";
+                ctx.font = "70px Arial black";
+                ctx.fillText((event.key).toUpperCase(),330 + (81*i),590);
+                acertos++;
+            }
+        }
+    }
+
+    else{
+        letrasRepetidas();
+        erros++;
+        boneco.desenhaBoneco(erros);
+    }
+}
+
+function letrasRepetidas(){
+    if(!tentativas.includes(event.key)){
+        tentativas = tentativas + event.key;
+        ctx.fillStyle = "#000";
+        ctx.font = "32px Arial";
+        ctx.fillText("Letras já escolhidas: " + tentativas.toUpperCase(),30,750);
+    }
+}
+
 cabo();
+linhasLetra();
+
+document.addEventListener('keydown', function(event){
+    if(event.keyCode >= 65 && event.keyCode <= 90){
+        letraEscolhida();
+    }
+});
